bc90c8d852439fe22f7a566878f7f8bf41c248e8
Hua <a47894785@gmail.com>
2021-12-16 21:52:54 +0800

    first commit

---
+++ Gomoku_client.py
@@ -0,0 +1,247 @@
+import pygame
+import sys
+import numpy as np
+import time
+from pygame.locals import *
+import xmlrpc.client
+
+PORT = 8888
+# 視窗長寬
+width = 900
+height = 600
+# 顏色
+win_color = [255, 165, 79]
+black = [0, 0, 0]
+white = [255, 255, 255]
+button_on = [176, 196, 222]
+button_off = [119, 136, 153]
+# flags
+clicked = False
+press_flag = 0
+press_flag1 = 0
+# 棋子位置
+chess_pos = []
+# 建立pygame視窗
+win = pygame.display.set_mode((width, height))
+win_temp = pygame.display.set_mode((width, height))
+pygame.display.set_caption('Gomoku Client')
+# Restart矩形
+rect1 = pygame.Rect(700, 200, 100, 50)
+rect2 = pygame.Rect(300, 200, 200, 100)
+text = ["Input the username : ", "Input your password: "]  # 帳號 以及 密碼
+text_username = ""  # 存入帳號
+text_password = ""  # 存入密碼
+
+
+class Player():
+    def __init__(self, id, name, ip, port, color):
+        self.id = id
+        self.name = name
+        self.ip = ip
+        self.port = port
+        self.color = color  # 棋子顏色
+        self.is_host = False  # 是否為房主
+
+    def posInfo(self, x, y):
+        for i in range(20, 600, 40):
+            for j in range(20, 600, 40):
+                L1 = i - 20
+                L2 = i + 20
+                R1 = j - 20
+                R2 = j + 20
+                if x >= L1 and x <= L2 and y >= R1 and y <= R2:
+                    return i, j
+        return x, y
+
+    def set_host(self, isHost):
+        self.is_host = isHost
+
+    def get_color(self):
+        color = self.color
+        return color
+
+    def set_color(self, color):
+        self.color = color
+
+    def get_isHost(self):  # 回傳是否為房主
+        host = self.is_host
+        return host
+
+    def check_pos(self, x, y):  # 確認可否落棋
+        global chess_pos
+        for pos in chess_pos:
+            if pos[0][0] == x and pos[0][1] == y:
+                return False
+        return True
+
+    def drawChessBoard(self):  # 繪製棋盤畫面
+        win.fill(win_color)
+        for i in range(20, 600, 40):
+            if i == 20 or i == 580:
+                pygame.draw.line(win, black, [i, 20], [i, 580], 4)
+                pygame.draw.line(win, black, [20, i], [580, i], 4)
+            else:
+                pygame.draw.line(win, black, [i, 20], [i, 580], 2)
+                pygame.draw.line(win, black, [20, i], [580, i], 2)
+        pygame.draw.circle(win, black, [300, 300], 6, 0)
+        pygame.draw.circle(win, black, [140, 140], 6, 0)
+        pygame.draw.circle(win, black, [140, 460], 6, 0)
+        pygame.draw.circle(win, black, [460, 460], 6, 0)
+        pygame.draw.circle(win, black, [460, 140], 6, 0)
+        if self.is_host:
+            pygame.draw.rect(win, button_on, rect1)
+        else:
+            pygame.draw.rect(win, button_off, rect1)
+        # 顯示文字
+        font = pygame.font.SysFont("", 35)
+        text1 = font.render('Restart', True, black)
+        if self.color == black:
+            text2 = font.render('BLACK', True, black)
+        elif self.color == white:
+            text2 = font.render('WHITE', True, white)
+        text3 = font.render('You are room host', True, black)
+        win.blit(text1, (707, 213))
+        win.blit(text2, (700, 20))
+        if self.is_host:
+            win.blit(text3, (620, 350))
+
+    def drawChess(self):  # 在棋盤上繪製棋子
+        global chess_pos
+        for chess in chess_pos:
+            pygame.draw.circle(win, chess[1], chess[0], 18, 0)
+
+    def get_color(self):  # 回傳棋子顏色
+        myColor = self.color
+        return myColor
+
+    def printWin(self, side):  # 顯示勝利方
+        font = pygame.font.SysFont("", 35)
+        if side == 0:
+            text = font.render('BLACK WIN', True, black)
+        elif side == 1:
+            text = font.render('WIHTE WIN', True, white)
+        win.blit(text, (670, 400))
+
+
+def main():
+    global chess_pos, clicked, press_flag, press_flag1
+    if (len(sys.argv) < 3):
+        print('Usage: python Gomoku_client.py ServerIP name')
+        exit(1)
+    server = xmlrpc.client.ServerProxy(
+        'http://' + sys.argv[1] + ':' + str(PORT))
+    run = True
+    color = server.get_color()  # 從伺服器取得棋子顏色
+    # is_host = server.is_host(sys.argv[2])  # 伺服器回傳是否為房主
+    # if is_host:
+    #     print('You are the room host')
+    # 伺服器回傳False表示已滿兩人
+    if color == False:
+        print('Room is full')
+        exit(1)
+    print(color)
+    # 印出黑色或白色方
+    if color == white:
+        print('You are white side')
+    else:
+        print('You are black side')
+    pygame.init()
+    # 建立物件
+    player = Player('abcd', sys.argv[2], '127.0.0.1', 8888, color)
+
+    run_write = True
+    win_temp.fill(win_color)
+    global text, text_password, text_username
+    index = 0
+    # while run_write:
+    #     pygame.draw.rect(win_temp, button_on, rect2)
+    #     font = pygame.font.SysFont("", 35)
+    #     text1 = font.render(text[0], True, black)
+    #     text2 = font.render(text[1], True, black)
+    #     win.blit(text1, (301, 201))
+    #     win.blit(text2, (301, 401))
+    #     pygame.display.update()
+    #     for event in pygame.event.get():
+    #         if event.type == QUIT or index == 2:
+    #             run_write = False
+    #         if event.type == KEYDOWN:
+    #             if event.key == pygame.K_RETURN:
+    #                 index += 1
+    #             else:
+    #                 if index == 0:
+    #                     text[index] += event.unicode
+    #                     text_username += event.unicode
+    #                 elif index == 1:
+    #                     text[index] += '*'
+    #                     text_password += event.unicode
+    #             if index == 2:
+    #                 print(text_username + '\n' + text_password)
+
+    while run:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                quit = server.quit(player.get_color())
+                print(quit)
+                if player.is_host:
+                    server.set_host_false()
+                sys.exit()
+            elif event.type == MOUSEBUTTONDOWN:
+                clicked = True
+            elif event.type == MOUSEBUTTONUP:
+                clicked = False
+                press_flag = 0  # 棋盤上
+                press_flag1 = 0  # Restart
+        is_host = server.is_host(sys.argv[2])
+        # print(is_host)
+        player.set_host(is_host)
+        player.drawChessBoard()  # 繪製棋盤
+        chess_pos = server.get_chess_pos()  # 取得棋子位置
+        player.drawChess()  # 繪製棋子
+        result = server.check_win()  # 判斷勝負
+        winSide = server.get_winSide()  # 取得勝利方 init = -1, black = 0, white = 1
+        # 若有一方勝利且棋局尚未結束，先將棋局結束後印出勝利方
+        if winSide != -1 and not server.is_end():
+            if winSide == 0:
+                print('Black Win')
+            else:
+                print('White Win')
+            if is_host:  # 若是房主則向伺服器請求將棋局結束
+                server.game_end(True)
+        if winSide != -1:
+            player.printWin(winSide)
+        # 取得滑鼠座標
+        x, y = pygame.mouse.get_pos()
+        if x <= 600:  # 在棋盤範圍內
+            x0, y0 = player.posInfo(x, y)
+            if player.check_pos(x0, y0):
+                pygame.draw.rect(win, [255, 0, 0], [
+                                 x0 - 20, y0 - 20, 40, 40], 2, 1)
+
+        chess_pos = server.get_chess_pos()  # 取得棋子位置
+        if x <= 600:  # 在棋盤範圍內
+            if not server.is_end():
+                if clicked and press_flag == 0:
+                    if player.check_pos(x0, y0):
+                        # 根據棋子顏色判斷是否為我方下棋
+                        if (server.dropChess(x0, y0, player.get_color())):
+                            print('Drop chess in [%d, %d]' % (x0, y0))
+                        else:
+                            print('Not your turn')
+                    press_flag = 1
+        # Restart 按鈕
+        elif 700 <= x <= 800 and 200 <= y <= 250:
+            if player.get_isHost() and clicked and press_flag1 == 0:
+                if server.game_reset():
+                    server.game_end(False)
+                    color = server.get_color()
+                    print('Restart')
+                press_flag1 = 1
+        if player.get_isHost() == False:
+            color = server.get_color()
+        if color:
+            player.set_color(color)
+        pygame.display.update()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Gomoku_server.py
@@ -0,0 +1,256 @@
+from xmlrpc.server import SimpleXMLRPCServer
+from socketserver import ThreadingMixIn
+import time
+import threading
+import numpy as np
+import random
+
+PORT = 8888
+white = [255, 255, 255]
+black = [0, 0, 0]
+
+
+class ThreadXMLRPCServer(ThreadingMixIn, SimpleXMLRPCServer):
+    pass
+
+
+class Gomoku:
+    def __init__(self):
+        self.lock = threading.Lock()
+        self.chess_pos = []
+        self.color = [white, black]  # 預設list含有white,black
+        self.host = ''
+        self.have_host = False
+        self.end = False
+        self.reset = False
+        self.winSide = -1
+
+    def set_host_false(self):
+        self.lock.acquire()
+        self.have_host = False
+        self.host = ""
+        self.lock.release()
+        return True
+
+    def quit(self, color):
+        print("color list: " + str(self.color))
+        self.lock.acquire()
+        self.color.append(color)
+        self.lock.release()
+        print("color list: " + str(self.color))
+        return True
+
+    def get_reset(self):
+        reset = self.reset
+        return reset
+
+    def set_reset(self, flag):
+        self.reset = flag
+
+    def get_end(self):
+        end = self.end
+        return end
+
+    def is_end(self):
+        end = self.end
+        return end
+    # 回傳勝利方 init = -1, black = 0, white = 1
+
+    def get_winSide(self):
+        winside = self.winSide
+        return winside
+    # 設定棋局是否結束
+
+    def game_end(self, value):
+        self.lock.acquire()
+        if value == True:
+            self.end = True
+        else:
+            self.end = False
+        self.lock.release()
+        return True
+    # 給予房主的權限
+
+    def is_host(self, name):
+        if self.have_host == False:  # 尚未有房主，指定房主(第一個連到Server者)
+            self.lock.acquire()
+            self.host = name
+            self.have_host = True
+            self.lock.release()
+            return True
+        elif self.host == name:
+            return True
+        else:  # 已經有房主回傳False
+            return False
+    # 隨機分配棋子顏色
+
+    def get_color(self):
+        if len(self.color) == 2:  # 尚未分配任何一個棋子
+            randnum = random.randint(0, 1)
+            tmp = self.color[randnum]
+            self.lock.acquire()
+            self.color.remove(tmp)  # 從list移除該顏色
+            self.lock.release()
+            return tmp
+        elif len(self.color) == 1:  # 已分配一個顏色
+            tmp = self.color[0]
+            self.lock.acquire()
+            self.color.remove(tmp)  # 從list移除該顏色，之後color list為空
+            self.lock.release()
+            return tmp
+        else:  # 沒有顏色可以分配(color list為空)
+            return False
+
+    # 根據player的顏色落棋
+    def dropChess(self, x0, y0, color):
+        if len(self.chess_pos) % 2 == 0 and color == black:
+            self.lock.acquire()
+            self.chess_pos.append([[x0, y0], black])
+            self.lock.release()
+            return True
+        elif len(self.chess_pos) % 2 == 1 and color == white:
+            self.lock.acquire()
+            self.chess_pos.append([[x0, y0], white])
+            self.lock.release()
+            return True
+        else:
+            return False
+
+    # 回傳當前各棋子位置
+    def get_chess_pos(self):
+        pos = self.chess_pos
+        return pos
+
+    # 判斷勝負
+    def check_win(self):
+        map = np.zeros([15, 15], dtype=int)
+        for val in self.chess_pos:
+            x = int((val[0][0]-20)/40)
+            y = int((val[0][1]-20)/40)
+            if val[1] == white:
+                map[x][y] = 2  # 表示白子
+            else:
+                map[x][y] = 1  # 表示黑子
+
+        for i in range(15):
+            pos1 = []
+            pos2 = []
+            for j in range(15):
+                if map[i][j] == 1:
+                    pos1.append([i, j])
+                else:
+                    pos1 = []
+                if map[i][j] == 2:
+                    pos2.append([i, j])
+                else:
+                    pos2 = []
+                if len(pos1) >= 5:  # 五子連心
+                    self.lock.acquire()
+                    self.winSide = 0
+                    self.lock.release()
+                    return [1, pos1]
+                if len(pos2) >= 5:
+                    self.lock.acquire()
+                    self.winSide = 1
+                    self.lock.release()
+                    return [2, pos2]
+
+        for j in range(15):
+            pos1 = []
+            pos2 = []
+            for i in range(15):
+                if map[i][j] == 1:
+                    pos1.append([i, j])
+                else:
+                    pos1 = []
+                if map[i][j] == 2:
+                    pos2.append([i, j])
+                else:
+                    pos2 = []
+                if len(pos1) >= 5:
+                    self.lock.acquire()
+                    self.winSide = 0
+                    self.lock.release()
+                    return [1, pos1]
+                if len(pos2) >= 5:
+                    self.lock.acquire()
+                    self.winSide = 1
+                    self.lock.release()
+                    return [2, pos2]
+        for i in range(15):
+            for j in range(15):
+                pos1 = []
+                pos2 = []
+                for k in range(15):
+                    if i+k >= 15 or j+k >= 15:
+                        break
+                    if map[i+k][j+k] == 1:
+                        pos1.append([i+k, j+k])
+                    else:
+                        pos1 = []
+                    if map[i+k][j+k] == 2:
+                        pos2.append([i+k, j+k])
+                    else:
+                        pos2 = []
+                    if len(pos1) >= 5:
+                        self.lock.acquire()
+                        self.winSide = 0
+                        self.lock.release()
+                        return [1, pos1]
+                    if len(pos2) >= 5:
+                        self.lock.acquire()
+                        self.winSide = 1
+                        self.lock.release()
+                        return [2, pos2]
+        for i in range(15):
+            for j in range(15):
+                pos1 = []
+                pos2 = []
+                for k in range(15):
+                    if i+k >= 15 or j-k < 0:
+                        break
+                    if map[i+k][j-k] == 1:
+                        pos1.append([i+k, j-k])
+                    else:
+                        pos1 = []
+                    if map[i+k][j-k] == 2:
+                        pos2.append([i+k, j-k])
+                    else:
+                        pos2 = []
+                    if len(pos1) >= 5:
+                        self.lock.acquire()
+                        self.winSide = 0
+                        self.lock.release()
+                        return [1, pos1]
+                    if len(pos2) >= 5:
+                        self.lock.acquire()
+                        self.winSide = 1
+                        self.lock.release()
+                        return [2, pos2]
+        return [0, []]
+
+    # 重新開始棋局，清空chess_pos並將winSide設回初始值
+    def game_reset(self):
+        self.lock.acquire()
+        self.chess_pos.clear()
+        self.winSide = -1
+        self.color = [white, black]
+        self.reset = True
+        self.lock.release()
+        return True
+
+
+def main():
+    obj = Gomoku()
+    server = ThreadXMLRPCServer(('', PORT))
+    server.register_instance(obj)
+    print('Listening on port %d' % (PORT))
+    try:
+        print('Use Control-C to exit!')
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print('Server exit')
+
+
+if __name__ == '__main__':
+    main()
